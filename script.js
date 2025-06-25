bb08386518cf3e9cf8077803c0601775073d6044
Lucas <lucthe@alunos.utfpr.edu.br>
2025-06-25 20:30:42 -0300

    #6

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ alert ("Comando para mostrar um texto na tela, posso usar aspas duplas ou aspas 
 alert ("Ele é limitado ao clique no ok pelo usuário")
 alert ("Não é possível configurar seu tempo de permanência na tela")
 
-let numero = 25;
+let numero = 55;
 let chute;
 let idade;
 let nome;
